c0d4637957202455862a54f058df8b8fb1888bd2
Maddula Manoj Sri Sai <manoj.maddula24@sasi.ac.in>
2026-07-14 19:02:21 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-// const dns = require("dns")
-// dns.setServer(["8.8.8.8","8.8.4.4"])
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongodb+srv://Manoj:Manoj07@cluster0.j3rytew.mongodb.net/Manoj);
-
+    await mongoose.connect("mongodb+srv://Manoj:manoj9705@cluster0.j3rytew.mongodb.net/waterintake");
+    
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.log("Database Connection Error:", error.message);
--- server.js
+++ server.js
@@ -6,33 +6,30 @@ const waterRoutes = require("./routes/waterRoutes");
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection (without .env)
 mongoose
-  .connect("mongodb+srv://Manoj:manoj07@cluster0.j3rytew.mongodb.net/waterintake")
+  .connect(
+    "mongodb+srv://Manoj:manoj07@cluster0.j3rytew.mongodb.net/waterintake")
+  
   .then(() => {
-    console.log("Database Connected Successfully");
+    console.log("✅ Database Connected Successfully");
   })
   .catch((err) => {
-    console.log("Database Connection Failed");
+    console.log("❌ Database Connection Failed");
     console.log(err);
   });
 
-// Routes
 app.use("/api/water", waterRoutes);
 
-// Home Route
 app.get("/", (req, res) => {
   res.send("Water Tracker Backend Running...");
 });
 
-// Server Port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Start Server
-app.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+app.listen(PORT, () => {
+  console.log("mongodb+srv://Manoj:manoj9705@cluster0.j3rytew.mongodb.net/waterintake");
 });
